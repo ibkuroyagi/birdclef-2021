f35ce681508aee49450285d878574edcba305336
ibkuroyagi <ibkuroyagi@gmail.com>
2021-05-02 20:02:12 +0900

    fix for BCE2WayLoss

--- input/modules/losses/focal_loss.py
+++ input/modules/losses/focal_loss.py
@@ -1,38 +1,17 @@
-import torch
 import torch.nn as nn
 
 
-# https://www.kaggle.com/c/rfcx-species-audio-detection/discussion/213075
-class BCEFocalLoss(nn.Module):
-    def __init__(self, alpha=0.25, gamma=2.0):
+class BCE2WayLoss(nn.Module):
+    def __init__(self, weights=[1, 1], pos_weights=None):
         super().__init__()
-        self.alpha = alpha
-        self.gamma = gamma
-
-    def forward(self, preds, targets):
-        bce_loss = nn.BCEWithLogitsLoss(reduction="none")(preds, targets)
-        probas = torch.sigmoid(preds)
-        loss = (
-            targets * self.alpha * (1.0 - probas) ** self.gamma * bce_loss
-            + (1.0 - targets) * probas ** self.gamma * bce_loss
-        )
-        loss = loss.mean()
-        return loss
-
-
-class BCEFocal2WayLoss(nn.Module):
-    def __init__(self, weights=[1, 1], class_weights=None):
-        super().__init__()
-
-        self.focal = BCEFocalLoss()
-
+        self.bce = nn.BCEWithLogitsLoss(reduction="mean", pos_weights=pos_weights)
         self.weights = weights
 
     def forward(self, logit, frame_logit, target):
         target = target.float()
         clipwise_output_with_max, _ = frame_logit.max(dim=1)
 
-        loss = self.focal(logit, target)
-        aux_loss = self.focal(clipwise_output_with_max, target)
+        loss = self.bce(logit, target)
+        aux_loss = self.bce(clipwise_output_with_max, target)
 
         return self.weights[0] * loss + self.weights[1] * aux_loss
