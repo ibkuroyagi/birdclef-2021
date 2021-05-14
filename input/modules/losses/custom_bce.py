3203eb1885c56fce9ed65fe6b2843bd7c99867c3
ibkuroyagi <ibkuroyagi@gmail.com>
2021-05-15 00:55:59 +0900

    add MaskedBCE

--- input/modules/losses/custom_bce.py
+++ input/modules/losses/custom_bce.py
@@ -15,3 +15,19 @@ class BCE2WayLoss(nn.Module):
         aux_loss = self.bce(clipwise_output_with_max, target)
 
         return self.weights[0] * loss + self.weights[1] * aux_loss
+
+
+class BCEMasked(nn.Module):
+    def __init__(self, pos_weight=None):
+        super().__init__()
+        self.binary_cross_entropy_with_logits = nn.BCEWithLogitsLoss(
+            reduction="mean", pos_weight=pos_weight
+        )
+
+    def forward(self, inputs, targets, mask=None):
+        bce_loss = self.binary_cross_entropy_with_logits(
+            inputs, targets, reduction="none"
+        )
+        if mask is not None:
+            bce_loss = bce_loss[mask > 0]
+        return bce_loss.mean()
